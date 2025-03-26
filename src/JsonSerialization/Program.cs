79b517c7f0a3b2a8829edea1aec408d4f4189e74
Mike Rousos <mikerou@microsoft.com>
2025-03-26 17:25:07 -0400

    Add serialization sample

---
+++ src/JsonSerialization/Program.cs
@@ -0,0 +1,20 @@
+﻿using SerializationSample;
+
+var obj = new Serializer.DataTransferObject
+{
+    Name = "Joe Smith",
+    Age = 42
+};
+
+Console.WriteLine($"Created sample object: {obj}");
+Console.WriteLine();
+
+var str = Serializer.Serialize(obj);
+
+Console.WriteLine($"Serialized object: {str}");
+Console.WriteLine();
+
+var obj2 = Serializer.Deserialize<Serializer.DataTransferObject>(str);
+
+Console.WriteLine($"Deserialized object: {obj2}");
+Console.WriteLine();
